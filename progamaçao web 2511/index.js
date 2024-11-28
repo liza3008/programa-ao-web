6aa96e19c326221159b64a0dd0c1e04373f487b6
liza3008 <cristineelizabeth06@gmail.com>
2024-11-28 17:17:57 -0400

    Add files via upload

---
+++ progamaçao web 2511/index.js
@@ -0,0 +1,17 @@
+var setadireita = window.document.getElementById("setadireita")
+var leonardoeonardo = window.document.getElementById("Leonardo")
+var Bruna = window.document.getElementById("Bruna")
+var setaesquerda = window.document.getElementById("setaesquerda")
+
+function Deslizarparadireita(){
+    Leonardo.style="display:flex"
+    Bruna.style="display:none"
+    setadireita.style="display:flex; margin-top:50px"
+    setaesquerda.style="display:none"
+}
+function Deslizarparaesquerda(){
+    Leonardo.style="display:flex"
+    Bruna.style="display:none"
+    setadireita.style="display:flex; margin-top:50px"
+    setaesquerda.style="display:none"
+}
